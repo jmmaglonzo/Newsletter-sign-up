0ffad610d451472f1f13154a95b01292216aa781
Lonzo <jmmaglonzo.dev@gmail.com>
2023-08-27 08:45:22 +0800

    add email validation

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-"use strict";
+const submitBtn = document.querySelector(".btn");
+const dismissBtn = document.querySelector(".btn-2");
 
-const email = document.getElementById("email");
-const form = document.getElementById("form");
-const button = document.querySelector("btn");
-form.addEventListener("submit", (e) => {
-  if (email.value === "" || email.value === null) {
-    email.style.backgroundColor = "red";
+const inputField = document.getElementById("email");
+const errorMsg = document.querySelector(".error-message");
+const account = document.getElementById("account");
+
+const container = document.querySelector(".container");
+const success = document.querySelector(".success");
+const validateEmail = () => {
+  if (
+    !inputField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
+  ) {
+    errorMsg.textContent = "Valid email required";
+    inputField.style.backgroundColor = "#ffe8e6";
+    inputField.style.borderColor = "#ff3860";
+    inputField.style.color = "#ff3860";
+    errorMsg.style.color = "#ff3860";
+    errorMsg.classList.remove("active");
+  } else {
+    errorMsg.textContent = "";
+    container.classList.toggle("active");
+    success.classList.toggle("active");
+    account.textContent = inputField.value;
   }
+};
+
+submitBtn.addEventListener("click", () => {
+  validateEmail();
+});
+
+dismissBtn.addEventListener("click", () => {
+  container.classList.toggle("active");
+  success.classList.toggle("active");
 });
